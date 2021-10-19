dc57b8c8907b2c346485cad74852c3fdd919e5b4
Matt Tedder <mjtedder@icloud.com>
2021-10-19 14:52:56 -0600

    Adds second API call, and authentication using env variable

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const inquirer = require('inquirer');
+require('dotenv').config();
 
 function startApp() {
     const questions = [
@@ -26,18 +27,38 @@ async function getOpenPullRequestData(user, repo) {
     try {
         const response = await axios.get(`https://api.github.com/repos/${user}/${repo}/pulls`, {
             headers: {
+                'Authorization': process.env.client_secret,
                 'User-Agent': user,
                 'Accept': 'application/vnd.github.v3+json'
             }
         })
-        const openPRCount = response.data.filter(pullRequest => {
+        const openPullRequests = response.data.filter(pullRequest => {
             return pullRequest.state;
         })
-        console.log(`The repository ${repo} for user ${user} currently has ${openPRCount.length} open PRs`);
+        console.log(`The repository ${repo} for user ${user} currently has ${openPullRequests.length} open PRs`);
+        getCommitsPerPullRequest(user, repo, openPullRequests);
     } catch (error) {
         console.error(error);
     }
-    
+}
+
+function getCommitsPerPullRequest(username, repository, data) {
+    let returnedCommits = [];
+    let promises = [];
+    data.forEach(pullRequest => {
+        promises.push(
+            axios.get(`https://api.github.com/repos/${username}/${repository}/pulls/${pullRequest.number}/commits`, {
+                headers: {
+                    'Authorization': process.env.client_secret,
+                    'User-Agent': username,
+                    'Accept': 'application/vnd.github.v3+json'
+                }
+            }).then(response => {
+                returnedCommits.push(response.data)
+            })
+        )
+    })
+    Promise.all(promises).then(() => console.log(returnedCommits))
 }
 
 startApp();
